6bd5d2111d7b15e5dc58017e0a1c77ea16150d40
8BitJonny <timon99@gmx.de>
2018-09-12 14:34:33 +0200

    Added several Dependencies: - pure Datastore (Don't know if we still need it because we added the following package) - Objectify as a Java data access API specifically designed for the Google Cloud Datastore - JPA (just for a few persistent functions)

--- src/main/kotlin/com/triangl/customer/CustomerApplication.kt
+++ src/main/kotlin/com/triangl/customer/CustomerApplication.kt
@@ -1,11 +1,25 @@
 package com.triangl.customer
 
+import com.googlecode.objectify.ObjectifyFilter
+import com.googlecode.objectify.ObjectifyService
+import com.triangl.customer.entity.Customer
+import com.triangl.customer.entity.Map
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+
+@Component
+class ObjectifyWebFilter : ObjectifyFilter()
 
 @SpringBootApplication
 class CustomerApplication
-
 fun main(args: Array<String>) {
+
+
+    ObjectifyService.init()
+    ObjectifyService.register(Customer::class.java)
+    ObjectifyService.register(Map::class.java)
+
     runApplication<CustomerApplication>(*args)
+
 }
