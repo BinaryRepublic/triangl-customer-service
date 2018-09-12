75045115fb2a2628002337260d5ed08f5be8210a
8BitJonny <timon99@gmx.de>
2018-09-12 16:51:54 +0200

    - Changed the return Value Type  from the result to always using a HTTP.responseEntity - ResponseEntity.body is always a hashMap so it is possible to add return properties afterwards - Added multiple exceptions to catch cases like fun find() returns an Error because there is no Customer object with this Id in the datastore

--- src/main/kotlin/com/triangl/customer/controller/CustomerController.kt
+++ src/main/kotlin/com/triangl/customer/controller/CustomerController.kt
@@ -3,6 +3,7 @@ package com.triangl.customer.controller
 import com.triangl.customer.entity.Customer
 import org.springframework.web.bind.annotation.*
 import com.googlecode.objectify.ObjectifyService.ofy
+import org.springframework.http.ResponseEntity
 
 @RestController
 @RequestMapping("/customer")
@@ -10,27 +11,42 @@ class CustomerController {
 
     // Just for development - Remove it later
     @GetMapping("/all")
-    fun findAll(): List<Customer> {
-        return ofy().load().type(Customer::class.java).list()
+    fun findAll(): ResponseEntity<*> {
+
+        val result = ofy().load().type(Customer::class.java).list()
+        return ResponseEntity.ok().body(hashMapOf("customerList" to result))
     }
 
     @GetMapping("/{id}")
-    fun find(@PathVariable id: String): Customer {
-        return ofy().load().type(Customer::class.java).id(id).now()
+    fun find(@PathVariable id: String): ResponseEntity<*> {
+        val result = ofy().load().type(Customer::class.java).id(id).now()
+
+        return if (result == null) {
+            ResponseEntity.status(400).body(hashMapOf("error" to "Customer ID not found"))
+        } else {
+            ResponseEntity.ok().body(hashMapOf("customer" to result))
+        }
     }
 
     @PostMapping
-    fun createUser(@RequestBody name: String): Customer {
+    fun createUser(@RequestBody name: String): ResponseEntity<*> {
 
         val cust = Customer(name=name)
         ofy().save().entity(cust).now()
 
         val result: Customer = ofy().load().type(Customer::class.java).id(cust.id).now()
-        return result
+
+        return if (result == null) {
+            ResponseEntity.status(400).body(hashMapOf("error" to "Customer ID not found"))
+        } else {
+            ResponseEntity.ok().body(hashMapOf("customer" to result))
+        }
     }
 
     @DeleteMapping("/{id}")
-    fun deleteUser(@PathVariable id: String) {
+    fun deleteUser(@PathVariable id: String): ResponseEntity<*> {
         ofy().delete().type(Customer::class.java).id(id).now()
+
+        return ResponseEntity.ok().body(hashMapOf("deleted" to true))
     }
 }
