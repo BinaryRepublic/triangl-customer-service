b5cb4146105c07dd16f068703f646923cf4141cf
8BitJonny <timon99@gmx.de>
2018-09-12 15:31:01 +0200

    Registered the new Class Entities to Objectify

--- src/main/kotlin/com/triangl/customer/CustomerApplication.kt
+++ src/main/kotlin/com/triangl/customer/CustomerApplication.kt
@@ -2,8 +2,10 @@ package com.triangl.customer
 
 import com.googlecode.objectify.ObjectifyFilter
 import com.googlecode.objectify.ObjectifyService
+import com.triangl.customer.entity.Coordinate
 import com.triangl.customer.entity.Customer
 import com.triangl.customer.entity.Map
+import com.triangl.customer.entity.Router
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Component
@@ -19,6 +21,8 @@ fun main(args: Array<String>) {
     ObjectifyService.init()
     ObjectifyService.register(Customer::class.java)
     ObjectifyService.register(Map::class.java)
+    ObjectifyService.register(Router::class.java)
+    ObjectifyService.register(Coordinate::class.java)
 
     runApplication<CustomerApplication>(*args)
 
