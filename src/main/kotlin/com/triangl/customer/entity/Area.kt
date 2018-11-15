a42da197ff4c4f54b108c316cbadc437c39aa662
8BitJonny <timon99@gmx.de>
2018-11-15 13:43:14 +0100

    Added a new map property for areas

--- src/main/kotlin/com/triangl/customer/entity/Area.kt
+++ src/main/kotlin/com/triangl/customer/entity/Area.kt
@@ -2,26 +2,15 @@ package com.triangl.customer.entity
 
 import java.time.Instant
 import java.util.*
-import javax.persistence.Entity
 
-@Entity
-class Map (
+class Area (
     var id: String? = null,
 
-    var name: String? = null,
+    var vertices: List<Coordinate>? = null,
 
-    var svgPath: String? = null,
+    var createdAt: String? = null,
 
-    var size: Coordinate? = null,
-
-    var areas: List<Area>? = null,
-
-    var router: List<Router>? = null,
-
-    var lastUpdatedAt: String? = null,
-
-    var createdAt: String? = null
-
+    var lastUpdatedAt: String? = null
 ) {
     init {
         id = id ?: UUID.randomUUID().toString()
